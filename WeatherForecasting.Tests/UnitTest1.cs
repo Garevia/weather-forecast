97a35d65ea006c9537f8741de974dbcf1ce1c6f4
Hedgehog <tbalanyan@gmail.com>
2025-05-24 23:29:11 +0400

    Add .gitignore file

---
+++ WeatherForecasting.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace WeatherForecasting.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
--- WeatherForecasting/Infrastructure/WeatherProviders/OpenWeatherMapClient/OpenWeatherApiEndpoints.cs
+++ WeatherForecasting/Infrastructure/WeatherProviders/OpenWeatherMapClient/OpenWeatherApiEndpoints.cs
@@ -1,6 +1,6 @@
 namespace WeatherForecasting.Infrastructure.WeatherProviders.OpenWeatherMapClient;
 
-public static class WeatherApiEndpoints
+public static class OpenWeatherApiEndpoints
 {
     public const string CurrentWeatherByCity = "/data/2.5/weather?q={0},{1}&units=metric&appid={2}";
 
@@ -8,7 +8,9 @@ public static class WeatherApiEndpoints
         "/data/2.5/weather?lat={0}&lon={1}&units=metric&appid={2}";
     
     public const string Forecast5Day = "/data/2.5/forecast?lat={0}&lon={1}&units=metric&appid={2}";
+    
     public const string Forecast5DayByCity = "data/2.5/forecast?q={0},{1}&appid={2}&units=metric";
+    
     public const string GeocodingDirect = "geo/1.0/direct?q={0},{1}&limit=1&appid={2}";
 }
 
